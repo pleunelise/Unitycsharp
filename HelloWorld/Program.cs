d2b3398b32ea6767ed0c1f0c3929660fb8bf353e
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2019-04-09 15:34:43 +0200

    opdracht koe

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\   ^__^");
+            Console.WriteLine(" \\  (oo)\\_______");
+            Console.WriteLine("    (__)\\       )\\/\\");
+            Console.WriteLine("      ||----w |");
+            Console.WriteLine("      ||     ||");
+
+            Console.ReadLine();
+        }
+    }
+}
