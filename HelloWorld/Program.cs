831d70d523da792a1437e1a0954cbb6a571766c2
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2019-04-18 09:44:52 +0200

    eerste 20

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,34 +7,19 @@ namespace Korting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wil je de volgende getallen '+', '-', 'x' of '/' doen?");
-            string input = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Geef een getal:");
-            int getal1 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Geef nog een gatal:");
-            int getal2 = Convert.ToInt16(Console.ReadLine());
-            if (input == "+")
-            { 
-                int result1 = getal1 + getal2;
-                Console.Write("{0} + {1} = {2}", getal1, getal2, result1);
-            }
-            else if (input == "-")
+            int getal1 = 0;
+            int getal2 = 1;
+            int i = 0;
+            while (i <= 20)
             {
-                int result2 = getal1 - getal2;
-                Console.Write("{0} - {1} = {2}", getal1, getal2, result2);
-            }
-            else if (input == "x")
-            { 
-                int result3 = getal1 * getal2;
-                Console.Write("{0} * {1} = {2}", getal1, getal2, result3);
-            }
-            else if (input == "/")
-            { 
-                int result4 = getal1 / getal2;
-                Console.Write("{0} / {1} = {2}", getal1, getal2, result4);
+                int result = getal1 + getal2;
+                Console.WriteLine(result);
+                getal1 = getal2;
+                getal2 = result;
+                i++;
             }
 
-            Console.ReadLine();
+
         }
     }
 }
