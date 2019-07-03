faabdc62e4773d868f7291c247aee882408f744c
pleun <pleunelise.emmelot@coderclass.nl>
2019-07-03 22:12:50 +0200

    Porogramma mobieltje kopen klaar

--- HelloWorld/Mobiel.cs
+++ HelloWorld/Mobiel.cs
@@ -7,12 +7,21 @@ namespace HelloWorld
         public string type;
         public string model;
         public string OS;
+        public int prijs;
+        const float btw = 0.21f;
 
-        public Mobiel(string nType, string nModel, string nOS)
+
+        public Mobiel(string nType, string nModel, string nOS, int nPrijs)
         {
             type = nType;
             model = nModel;
             OS = nOS;
+            prijs = nPrijs;
         }
+        public float Btw(int aantal)
+        {
+            return aantal * prijs + ((aantal * prijs) * btw);
+        }
+
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,9 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Mobiel mobiel1 = new Mobiel("iPhone", "iPhone XS", "Apple IOS");
-            Mobiel mobiel2 = new Mobiel("Pixel", "Pixel3", "Andriod");
-            Mobiel mobiel3 = new Mobiel("Samsung", "Samsung A5", "AndriodOne");
+            float totaalIncBtw = 0.0f;
+            Mobiel mobiel1 = new Mobiel("iPhone", "iPhone XS", "Apple IOS", 1159);
+            Mobiel mobiel2 = new Mobiel("Pixel", "Pixel3", "Andriod", 949);
+            Mobiel mobiel3 = new Mobiel("Samsung", "Samsung A50", "AndriodOne", 299);
 
             Mobiel[] mobieltjes = new Mobiel[3];
             mobieltjes[0] = mobiel1;
@@ -18,22 +19,44 @@ namespace HelloWorld
             foreach (Mobiel m in mobieltjes)
             {
                 Console.Write(m.type);
+               
                 switch (m.type)
                 {
                     case "iPhone":
-                        Console.Write(" Steve Jobs");
+                        Console.Write(" = " + mobiel1.prijs + " exclusief BTW");
                         break;
                     case "Pixel":
-                        Console.Write(" Sundar Pichai");
+                        Console.Write(" = " + mobiel2.prijs + " exclusief BTW");
                         break;
                     case "Samsung":
-                        Console.Write(" Kim Ki Nam");
+                        Console.Write(" = " + mobiel3.prijs + " exclusief BTW");
                         break;
 
                 }
                 Console.WriteLine();
+                
+            }
+            Console.WriteLine("!Gratis verzenging vanaf 350 euro!");
+            Console.WriteLine("Welk mobieltje wil je kopen?:");
+            string mobieltje = Console.ReadLine();
+            Console.WriteLine("Hoeveel " + mobieltje + " wil je?");
+            int aantal = Convert.ToInt32(Console.ReadLine());
+
+            if (mobieltje == "iPhone")
+            {
+                totaalIncBtw = mobiel1.Btw(aantal);
             }
 
+            else if (mobieltje == "Pixel")
+            {
+                totaalIncBtw = mobiel2.Btw(aantal);
+            }
+            
+            else if (mobieltje == "Samsung")
+            {
+                totaalIncBtw = mobiel3.Btw(aantal);
+            }
+            Console.WriteLine("De totale prijs inclusief BTW = " + totaalIncBtw + " euro");
         }
     }
 }
