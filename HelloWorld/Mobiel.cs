301d31022245998d576ba25884bc6642625f7511
pleun <pleunelise.emmelot@coderclass.nl>
2019-07-03 21:17:50 +0200

    m.Types with CEO' s

--- HelloWorld/Mobiel.cs
+++ HelloWorld/Mobiel.cs
@@ -7,5 +7,12 @@ namespace HelloWorld
         public string type;
         public string model;
         public string OS;
+
+        public Mobiel(string nType, string nModel, string nOS)
+        {
+            type = nType;
+            model = nModel;
+            OS = nOS;
+        }
     }
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,17 +4,35 @@ namespace HelloWorld
 {
     class Program
     {
-    
         static void Main(string[] args)
         {
-            Mobiel mobiel = new Mobiel();
-            mobiel.type = "iPhone";
-            mobiel.model = "iPhone XS";
-            mobiel.OS = "Apple IOS";
+            Mobiel mobiel1 = new Mobiel("iPhone", "iPhone XS", "Apple IOS");
+            Mobiel mobiel2 = new Mobiel("Pixel", "Pixel3", "Andriod");
+            Mobiel mobiel3 = new Mobiel("Samsung", "Samsung A5", "AndriodOne");
 
-            Console.WriteLine(mobiel.type);
+            Mobiel[] mobieltjes = new Mobiel[3];
+            mobieltjes[0] = mobiel1;
+            mobieltjes[1] = mobiel2;
+            mobieltjes[2] = mobiel3;
 
-            Console.ReadLine();
+            foreach (Mobiel m in mobieltjes)
+            {
+                Console.Write(m.type);
+                switch (m.type)
+                {
+                    case "iPhone":
+                        Console.Write(" Steve Jobs");
+                        break;
+                    case "Pixel":
+                        Console.Write(" Sundar Pichai");
+                        break;
+                    case "Samsung":
+                        Console.Write(" Kim Ki Nam");
+                        break;
+
+                }
+                Console.WriteLine();
+            }
 
         }
     }
